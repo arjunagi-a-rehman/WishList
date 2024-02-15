2e30c5db644dc76cdf1d7101724a45cd83bfeabc
Arjunagi A Rehman <abdul123arj@gmail.com>
2024-02-16 00:15:49 +0530

    setted up basic structure

---
+++ src/main/java/com/example/UserWishList/UserWishListApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserWishList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWishListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserWishListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UserWishList/UserWishListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserWishList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserWishListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
